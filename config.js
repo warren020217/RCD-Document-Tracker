de9049b972352bf8894c7663b3e3046ea962b0ad
JTDR <johnwarrendelosreyes@gmail.com>
2026-08-20 16:41:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.RCD_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzHugvrj1wVLXJV_ov9iUoeC9JL9M55fXRpbVLIHauMHkd2lpUNgb6KqPHLE53-Ed93gg/exec",
+  API_URL: "/api/rcd",
   APP_URL: "https://rcd-document-tracker.vercel.app",
   APP_NAME: "PRO4A RCD Document Routing System"
 };
